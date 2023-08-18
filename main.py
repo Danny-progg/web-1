b346bfc59d7426d1ed0ab807837b118abdbe1098
Danny-progg <122675148+Danny-progg@users.noreply.github.com>
2023-08-19 00:06:45 +0700

    1 commit - start

---
+++ main.py
@@ -0,0 +1,29 @@
+import urllib.request
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+hostName = "localhost"
+serverPort = 8080
+
+
+class MyServer(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        response = urllib.request.urlopen("catalog.html").read()
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(response)
+
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
